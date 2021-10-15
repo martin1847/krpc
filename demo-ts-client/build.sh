fbfb13eb73d1d4703b4113335778f9aa21b6db9a
yangyang.cyy <yangyang.cyy@alibaba-inc.com>
2021-10-16 00:59:14 +0800

    build grpc typescript

---
+++ demo-ts-client/build.sh
@@ -0,0 +1,5 @@
+docker run --rm -it --mount type=bind,source="$(pwd)"/ts-example,dst=/tmp/ts-example \
+ -w /tmp/ts-example grpcweb/prereqs:1.3.0 bash
+
+#   npm install ;
+#   npm link grpc-web &&  tsc &&    npx webpack

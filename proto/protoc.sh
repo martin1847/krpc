05caeeedb2442ab8bc0d0864f11c8a052e5ec37c
Martin.Cong <martin.cong@bestulearn.com>
2021-12-28 18:12:13 +0800

    dart rpcurl

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -1,4 +1,17 @@
+
+# grpc-web use the docker compile
 docker run --rm -it --mount type=bind,source="$(pwd)",dst=/tmp/proto \
  -w /tmp/proto grpcweb/prereqs:1.3.0 bash
 
- # protoc  --java_out=outj internal.proto
+
+# dart-docker
+# https://github.com/robojones/protoc-dart
+docker run --rm -it --mount type=bind,source="$(pwd)",dst=/tmp/proto \
+ -w /tmp/proto robojones/protoc-dart:latest bash
+
+mkdir -p generated && protoc --dart_out=grpc:generated internal.proto
+
+# java mac protoc or docker
+# Linux: apt-get install protobuf-compiler
+# Mac homebrew: brew install protobuf
+# protoc  --java_out=outj internal.proto

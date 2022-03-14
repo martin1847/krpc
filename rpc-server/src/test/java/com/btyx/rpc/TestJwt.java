186330a80a3b49da59849681973cdd0de95aecbb
yangyang.cyy <yangyang.cyy@alibaba-inc.com>
2022-03-14 09:08:53 +0800

    jwt

--- rpc-server/src/test/java/com/btyx/rpc/TestJwt.java
+++ rpc-server/src/test/java/com/btyx/rpc/TestJwt.java
@@ -15,14 +15,20 @@ import io.grpc.StatusException;
 public class TestJwt {
 
     public static void main(String[] args) {
-        var url = TestJwt.class.getResource("/jwks.json").toString();
-        var vefiy = new JwsVerify(url);
-        var token = "eyJraWQiOiJiby10ZXN0LTIxMTEiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIxMjM0IiwiYWRtIjoxLCJleHAiOjE2MzcwNzkwMzN9.B-BsfO6THnHD-7z3afSZm4vhZVrigM0EpwdZ3VvcTbXoktSur8-2TRdWuUl9Hdgyfg9TvhdoR9UNPXK6VPddlw";
-        try {
-            vefiy.verify(token, "xxx",false);
-        }catch (StatusException e){
-            e.printStackTrace();
-        }
+        //var url = TestJwt.class.getResource("/jwks.json").toString();
+        //var vefiy = new JwsVerify(url);
+        //var token = "eyJraWQiOiJiby10ZXN0LTIxMTEiLCJhbGciOiJFUzI1NiJ9.eyJzdWIiOiIxMjM0IiwiYWRtIjoxLCJleHAiOjE2MzcwNzkwMzN9.B-BsfO6THnHD-7z3afSZm4vhZVrigM0EpwdZ3VvcTbXoktSur8-2TRdWuUl9Hdgyfg9TvhdoR9UNPXK6VPddlw";
+        //try {
+        //    vefiy.verify(token, "xxx",false);
+        //}catch (StatusException e){
+        //    e.printStackTrace();
+        //}
+        String[] a = new String[2];
+        Object[] b = a;
+        a[0] = "hi";
+        b[1] = Integer.valueOf(42);
+        System.out.println(b[1]);
+        System.out.println(a[1]);
     }
 
 }

55f26d8f7c1d6a80126e039697df0224a14b9acb
Martin.Cong <martin.cong@bestulearn.com>
2022-09-02 13:23:09 +0800

    add exports

---
+++ test-server/src/test/java/test/btyx/TestRef.java
@@ -0,0 +1,25 @@
+/**
+ * Botaoyx.com Inc.
+ * Copyright (c) 2021-2022 All Rights Reserved.
+ */
+package test.btyx;
+
+import com.bt.rpc.util.RefUtils;
+import com.btyx.test.dto.Img;
+import com.btyx.test.dto.SubImg;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author Martin.C
+ * @version 2022/09/02 1:13 PM
+ */
+public class TestRef {
+
+
+    @Test
+    void  testAnno(){
+        System.out.println(RefUtils.needValidator(Img.class));
+        System.out.println(RefUtils.needValidator(SubImg.class));
+    }
+}

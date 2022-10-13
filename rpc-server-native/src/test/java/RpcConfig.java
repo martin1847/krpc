2ec480ac971cb3d263e9ce536b3aa8a90c270ce2
Martin.Cong <martin.cong@bestulearn.com>
2022-10-13 14:12:47 +0800

    javacoco 1.5

--- rpc-server-native/src/test/java/RpcConfig.java
+++ rpc-server-native/src/test/java/RpcConfig.java
@@ -1,26 +1,26 @@
-import java.util.Optional;
-
-import io.smallrye.config.ConfigMapping;
-
-@ConfigMapping(prefix = "rpc.server")
-public interface RpcConfig {
-    /**
-     */
-    //Optional<String> globalFilter();
-
-
-    //@ConfigProperty
-    Optional<Integer> port();
-
-    /**
-     * app name
-     */
-    Optional<String> app() ;
-
-
-    Optional<String> jwks() ;
-
-    Optional<String> jwtCookie() ;
-
-    Optional<Boolean> exitOnJwksError() ;
-}
+//import java.util.Optional;
+//
+//import io.smallrye.config.ConfigMapping;
+//
+//@ConfigMapping(prefix = "rpc.server")
+//public interface RpcConfig {
+//    /**
+//     */
+//    //Optional<String> globalFilter();
+//
+//
+//    //@ConfigProperty
+//    Optional<Integer> port();
+//
+//    /**
+//     * app name
+//     */
+//    Optional<String> app() ;
+//
+//
+//    Optional<String> jwks() ;
+//
+//    Optional<String> jwtCookie() ;
+//
+//    Optional<Boolean> exitOnJwksError() ;
+//}

0e0e5d8b3952ff54157d5cbe444fcf8ac6750446
Martin.Cong <martin.cong@bestulearn.com>
2022-11-17 15:04:57 +0800

    node/pb format

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -3,8 +3,18 @@
 docker run --rm -it --mount type=bind,source="$(pwd)",dst=/tmp/proto \
  -w /tmp/proto grpcweb/prereqs:1.3.0 bash
 
+## for ts + proto FMT
 // protoc internal.proto  --js_out=import_style=commonjs,binary:js --grpc-web_out=import_style=typescript,mode=grpcweb:js
 // https://github.com/grpc/grpc-web#wire-format-mode
+
+## for nodejs
+npm install -g grpc-tools
+grpc_tools_node_protoc --js_out=import_style=commonjs,binary:js --grpc_out=grpc_js:js internal.proto
+
+
+## 纯TS版本，不依赖 protobuf ，https://github.com/timostamm/protobuf-ts
+npm install @protobuf-ts/plugin
+npx protoc --ts_out ./js --proto_path . ./internal.proto
 
 
 # dart-docker

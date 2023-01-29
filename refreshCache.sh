876e5fb1436dc87a1726f8fa706f55a71cb64952
Martin.Cong <martin.cong@bestulearn.com>
2023-01-29 11:44:49 +0800

    refresh

--- refreshCache.sh
+++ refreshCache.sh
@@ -1,2 +1,2 @@
-rm -rf ~/.gradle/caches/modules-2/files-2.1/com.bt*
-rm -rf ~/.gradle/caches/modules-2/metadata-2.97/descriptors/com.bt*
+rm -rf ~/.gradle/caches/modules-2/files-2.*/com.bt*
+rm -rf ~/.gradle/caches/modules-2/metadata-2.*/descriptors/com.bt*
